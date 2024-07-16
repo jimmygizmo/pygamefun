8ecf504583b0ca6a83754fddabb67217b79b48bd
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2024-07-16 11:58:32 -0700

    vectors have been implemented for velocity and are working nicely. currenlty maintaining x, y, xv, yv in object data as the more accurate full-float source of truth, BUT also calculating vector from xv, yv .. so next transitional step is to use the FRect and vector as full source of truth for greater convenience and simplicity of code but also for a lost of accuracy approximately on scale of one ten-thousandth. will leave code stubs in place to go back to original data full-accuracy source of truth later if desired. this is an experiment/transition and what we learn will carry us forward for all further PyGame-Ce development.

--- game/main.py
+++ game/main.py
@@ -10,6 +10,7 @@ import random
 
 SCREEN_WIDTH = 1280.0
 SCREEN_HEIGHT = 720.0
+TICKRATE = 60
 GAME_TITLE = 'Space Blasto'
 BGCOLOR = 'olivedrab'
 BGIMG = 'lawn-bg-dark-2560x1440.jpg'  # 'grass-field-med-1920x1249.jpg'  # 'lawn-bg-dark-2560x1440.jpg'
@@ -17,7 +18,8 @@ ASSET_PATH = 'assets'  # Relative path with no trailing slash.
 DEBUG = False
 
 
-# Using a TypedDict to satisfy MyPy recommendations. ('errors' more specifically.)
+# Using a TypedDict to satisfy MyPy recommendations for type-hinting/strong-typing.
+# TypeDict for MONSTER
 Monster = TypedDict('Monster', {
         'name': str,
         'img': str,
@@ -26,14 +28,14 @@ Monster = TypedDict('Monster', {
         'color': str,
         'x': float,
         'y': float,
+        'v': pygame.math.Vector2,
         'xv': float,
         'yv': float,
         'surface': pygame.Surface,
         'rect': pygame.FRect,
         })
 
-
-# MONSTER DATA
+# MONSTER DATA - Initial state for a handful of entities that move, experience physics and interact. W/initial motion.
 monsters = []
 monster: Monster = {'name': 'red-flower-floaty',
            'img':  'red-flower-66x64.png',
@@ -42,8 +44,9 @@ monster: Monster = {'name': 'red-flower-floaty',
            'color': 'red1',
            'x': 240.0,
            'y': 300.0,
-           'xv': -0.03,
-           'yv': 0.01,
+           'v': None,
+           'xv': -0.73,
+           'yv': 0.49,
            'surface': None,
            'rect': None,
            }
@@ -55,7 +58,8 @@ monster: Monster = {'name': 'red-flower-drifty',
            'color': 'orangered',
            'x': 240.0,
            'y': 300.0,
-           'xv': 0.032,
+           'v': None,
+           'xv': 0.132,
            'yv': -0.033,
            'surface': None,
            'rect': None,
@@ -68,8 +72,9 @@ monster: Monster = {'name': 'goldie',
            'color': 'gold',
            'x': 500.0,
            'y': 300.0,
-           'xv': 0.042,
-           'yv': -0.03,
+           'v': None,
+           'xv': 3.2,
+           'yv': 3.9,
            'surface': None,
            'rect': None,
            }
@@ -81,8 +86,9 @@ monster: Monster = {'name': 'fishy',
            'color': 'darkgoldenrod1',
            'x': 840.0,
            'y': 300.0,
-           'xv': -0.07,
-           'yv': -0.15,
+           'v': None,
+           'xv': -2.07,
+           'yv': -3.15,
            'surface': None,
            'rect': None,
            }
@@ -94,8 +100,9 @@ monster: Monster = {'name': 'grumpy',
            'color': 'blanchedalmond',
            'x': 780.0,
            'y': 300.0,
-           'xv': 0.11,
-           'yv': 0.04,
+           'v': None,
+           'xv': 2.11,
+           'yv': 1.04,
            'surface': None,
            'rect': None,
            }
@@ -117,7 +124,7 @@ PropTemplate = TypedDict('PropTemplate', {
         'rect': pygame.FRect,
         })
 
-# PROP DATA
+# PROP DATA - Initial state for a handful of non-moving props. Includes specs for random instantiation (spraying).
 prop_templates = []
 prop_template: PropTemplate = {'name': 'red-flower',
            'img':  'red-flower-66x64.png',
@@ -146,8 +153,7 @@ prop_template: PropTemplate = {'name': 'blue-flower',
            }
 prop_templates.append(prop_template)
 
-# TODO: We will need a TypedDict for the many PROP objects we generate (spray) as well.
-# TypedDict for PROP
+# TypedDict for PROP. Props are generated dynamically, when we "spray" props from their template.
 Prop = TypedDict('Prop', {
         'name': str,
         'img': str,
@@ -159,7 +165,6 @@ Prop = TypedDict('Prop', {
         'surface': pygame.Surface,
         'rect': pygame.FRect,
         })
-
 
 
 # ###############################################    INITIALIZATION    #################################################
@@ -175,14 +180,18 @@ for monster in monsters:
     if DEBUG:
         monster['surface'] = pygame.Surface((monster['w'], monster['h']))
         monster['surface'].fill(monster['color'])
+        # Instantiate the velocity vector with the TRUTH xv and yv values. This initial stub may soon change.
+        monster['v'] = pygame.math.Vector2(monster['xv'], monster['yv'])
     else:
         imgpath = os.path.join(ASSET_PATH, monster['img'])
         monster['surface'] = pygame.image.load(imgpath).convert_alpha()
+        # Instantiate the velocity vector with the TRUTH xv and yv values. This initial stub may soon change.
+        monster['v'] = pygame.math.Vector2(monster['xv'], monster['yv'])
 
     monster['rect'] = monster['surface'].get_frect(center=(monster['x'], monster['y']))
 
 
-# INITIALIZE PROPS - 'SPRAY' REPLICATED PROPS (randomly within specified radius)
+# INITIALIZE PROPS - 'SPRAY' REPLICATED PROPS (randomly within specified radius, to specified count)
 props = []
 for prop_t in prop_templates:
     for index in range(prop_t['spray_count']):  # We will use the index for a unique prop name. Not critical.
@@ -223,7 +232,10 @@ else:
     bg_surface = pygame.image.load(bgpath)
 
 running = True
+clock = pygame.time.Clock()
+
 while running:
+    clock.tick(TICKRATE)
     # #### ####   EVENT LOOP    #### ####
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -255,30 +267,62 @@ while running:
 
 
         # MOVE TRUE POSITION PER VELOCITY - Maintain the source of truth as FLOAT values in the primary data structure.
+        # monster['x'] += monster['xv']
+        # monster['y'] += monster['yv']
+        # newx = monster['x']
+        # newy = monster['y']
+        # monster['rect'].center = (newx, newy)
+
+        # NEW WAY USING VECTORS - While moving experimentally towards using the FRect for a less-accurate truth:
+
+        # We still need to maintain the truth values .. but, with FRect accuracy to 4 or 5 decimal places (float does
+        # remain much more accurat than that .. but 4 or 5 in FRect may be sufficient) .. then with that AND also
+        # the convenience of vectors, the plan is to EXPLORE the use of the rect to maintain truth. This will
+        # relegate x, y, xv, yv attributes to being initial state only and then after that the objects rect will be
+        # the source of truth. THIS WILL BE AN EXPERIMENT AT FIRST. We can always go back to maintaining truth in
+        # the fully-float-capable object data structures. Do we need accuracies on the scale of one-ten-thousandth of
+        # a pixel and greater? In some applications, absolutely! But in games? Maybe most of the time we do not need
+        # that accuracy and the many types of convenience of FRect/Rect in PyGame-CE will lead to the decision to
+        # use the FRect as source of truth (in most cases.) I can see things going that way, at least initially. I may
+        # leave concisely-commented stubs in place where one can easily go back to maintaining a much more accurate
+        # source of truth (on position and velocity, mostly, we are talking about here.)
         monster['x'] += monster['xv']
         monster['y'] += monster['yv']
-        newx = monster['x']
-        newy = monster['y']
-        monster['rect'].center = (newx, newy)
+        # Then we can very simply update position and don't necessarily need the above step to maintain the orig. truth.
+        # print(monster['v'])  # ----  DEBUG  ----
+        monster['rect'].center += monster['v']
 
 
         # Bounce off LEFT wall in X Axis
         if monster['rect'].left <= 0:
             monster['rect'].left = 0  # Great! We don't touch the TRUTH VALUE. We do bound the Surface on screen.
             monster['xv'] = monster['xv'] * -1
+            # Experimental transition to using vectors: We need to update the vector here too:
+            # IMPORTANT: Can/should we update using methods rather than instantiate a new/replacement object?
+            monster['v'] = pygame.math.Vector2(monster['xv'], monster['yv'])
+
         # Bounce off RIGHT wall in X Axis
         if monster['rect'].right >= SCREEN_WIDTH:
             monster['rect'].right = SCREEN_WIDTH  # Great! We don't touch the TRUTH VALUE. We do bound the Surface.
             monster['xv'] = monster['xv'] * -1
+            # Experimental transition to using vectors: We need to update the vector here too:
+            # IMPORTANT: Can/should we update using methods rather than instantiate a new/replacement object?
+            monster['v'] = pygame.math.Vector2(monster['xv'], monster['yv'])
 
         # Bounce off TOP wall in Y Axis
         if monster['rect'].top <= 0:
             monster['rect'].top = 0  # Great! We don't touch the TRUTH VALUE. We do bound the Surace on screen.
             monster['yv'] = monster['yv'] * -1
+            # Experimental transition to using vectors: We need to update the vector here too:
+            # IMPORTANT: Can/should we update using methods rather than instantiate a new/replacement object?
+            monster['v'] = pygame.math.Vector2(monster['xv'], monster['yv'])
         # Bounce off BOTTOM wall in Y Axis
         if monster['rect'].bottom >= SCREEN_HEIGHT:
             monster['rect'].bottom = SCREEN_HEIGHT  # Great! We don't touch the TRUTH VALUE. We do bound the Surface.
             monster['yv'] = monster['yv'] * -1
+            # Experimental transition to using vectors: We need to update the vector here too:
+            # IMPORTANT: Can/should we update using methods rather than instantiate a new/replacement object?
+            monster['v'] = pygame.math.Vector2(monster['xv'], monster['yv'])
 
 
 pygame.quit()
