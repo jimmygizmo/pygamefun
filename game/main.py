206854e5f01008e712ebbe976c4c0d82993ae6db
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2024-07-31 11:34:44 -0700

    added spaceship image for the player and re-added goldfish as a fifth NPC. added new Weapon/projectile class for the missiles etc fired by the player. this class is a clone of player by currently has controls and timer disabled as placeholders. custom weapon behavior will involve timers and initally a custom motion that is not controllable. later weapons might be steerable etc. It is really a projectile class but I prefer the word Weapon as its shorter and simpler and covers possibilities for more dynamic projectiles that might be more like soldier robots or attack drones, so at that point Weapon is a good name for this class. added new all_weapons (all projectiles) sprite group too.

--- game/main.py
+++ game/main.py
@@ -18,6 +18,7 @@ BGIMG = 'lawn-bg-dark-2560x1440.jpg'  # 'grass-field-med-1920x1249.jpg'  # 'lawn
 ASSET_PATH = 'assets'  # Relative path with no trailing slash.
 DEBUG = False
 ACID_MODE = False  # Suppress background re-painting. This makes objects leave psychedelic trails for a fun effect.
+LASER_COOLDOWN_DURATION = 100  # Milliseconds - minimum time between laser firing
 
 # List of tuples of the phase name and the phase duration in frames/iterations. collections.deque.popleft() is said
 # to be efficient at popping from the left side of a list. I'm just giving it a try. There are many ways to rotate a list.
@@ -55,23 +56,65 @@ PlayerSpec = TypedDict('PlayerSpec',
 # PLAYER DATA - Initial state for a single player (usually) or possibly multiple players.
 player_specs: list[PlayerSpec] = [
     {
-        'name': 'fishy',
+        'name': 'buck',
         'instance_id': -1,
-        'img':  'goldfish-280x220.png',
+        'img':  'rocket-200x252.png',
         'flip': False,
-        'w': 280,
-        'h': 220,
-        'color': 'darkgoldenrod1',
-        'x': 840.0,
-        'y': 300.0,
+        'w': 200,
+        'h': 252,
+        'color': 'aqua',
+        'x': 890.0,
+        'y': 540.0,
         'd': pygame.math.Vector2((-0.994, -0.114)),  # placeholder instance (mypy)
         's': 80.0,
         'p': 90.0,
         'r': 100.0,
         'c': 700.0,
-        'f': 2850.0,
+        'f': 1650.0,
     },
 ]  # player_specs: list[PlayerSpec]
+
+
+WeaponSpec = TypedDict('WeaponSpec',
+    {
+        'name': str,  # Weapon/projectile short name
+        'instance_id' : int,  # 0-based Int serial number unique to each instance of Weapon created. -1 means no instance created for this spec yet. (Jumping through MyPy hoops. Can't use None.) We are transitioning to OOP. This will all change.
+        'img': str,  # Filename of PNG (with transparency)
+        'flip': bool,  # If True, image will be flipped horizontally at the time of loading
+        'w': int,  # PNG pixel width
+        'h': int,  # PNG pixel height
+        'color': str,  # Debug mode color of rectangle
+        'x': float,  # Initial position X value
+        'y': float,  # Initial position Y value
+        'd': pygame.math.Vector2,  # Direction
+        's': float,  # Initial/default speed
+        'p': float,  # Enviro: Peace (speed)
+        'r': float,  # Enviro: Rogue (speed)
+        'c': float,  # Enviro: Chaos (speed)
+        'f': float,  # Enviro: Frozen (speed)
+    }
+)  # WeaponSpec
+
+# WEAPON/PROJECTILE DATA - Initial state for a weapon/projectile
+weapon_specs: list[WeaponSpec] = [
+    {
+        'name': 'orb',
+        'instance_id': -1,
+        'img':  'green-ball-140x140.png',
+        'flip': False,
+        'w': 140,
+        'h': 140,
+        'color': 'green3',
+        'x': 890.0,
+        'y': 260.0,
+        'd': pygame.math.Vector2((-0.994, -0.114)),  # placeholder instance (mypy)
+        's': 80.0,
+        'p': 90.0,
+        'r': 100.0,
+        'c': 700.0,
+        'f': 1650.0,
+    },
+]  # weapon_specs: list[WeaponSpec]
 
 
 NpcSpec = TypedDict('NpcSpec',
@@ -163,6 +206,23 @@ npc_specs: list[NpcSpec] = [
        'r': 50.0,
        'c': 2170.0,
        'f': 40.0,
+    },
+    {
+        'name': 'fishy',
+        'instance_id': -1,
+        'img':  'goldfish-280x220.png',
+        'flip': False,
+        'w': 280,
+        'h': 220,
+        'color': 'darkgoldenrod1',
+        'x': 840.0,
+        'y': 300.0,
+        'd': pygame.math.Vector2((-0.994, -0.114)),  # placeholder instance (mypy)
+        's': 80.0,
+        'p': 90.0,
+        'r': 100.0,
+        'c': 700.0,
+        'f': 2850.0,
     },
 ]  # npc_specs: list[NpcSpec]
 
@@ -318,8 +378,14 @@ class Player(Entity):
                  ):
         self.can_shoot: bool = True
         self.laser_shoot_time: int = 0
-        self.cooldown_duration: int = 700  # milliseconds
+        self.cooldown_duration: int = LASER_COOLDOWN_DURATION  # milliseconds
         super().__init__(groups, spec, x, y, direction, speed)  # super.update() could be done first before setting all the self.* but for now I have them last.
+
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()  # Milliseconds since pygame.init() was called.
+            if current_time - self.laser_shoot_time >= self.cooldown_duration:
+                self.can_shoot = True
 
     def update(self, delta_time: float, ephase_name: str):
         enviro_influence(self, ephase_name)
@@ -332,11 +398,58 @@ class Player(Entity):
 
         self.dir = self.dir.normalize() if self.dir else self.dir
 
-        if recent_keys[pygame.K_SPACE]:
+        if recent_keys[pygame.K_SPACE] and self.can_shoot:
             print('fire laser')
+            self.can_shoot = False
+            self.laser_shoot_time = pygame.time.get_ticks()
+
+        self.laser_timer()
 
         # NOTE: WE UPDATE BASED ON INPUT --BEFORE-- WE CHECK FOR WALL COLLISION/BOUNCING (in super/Entity).
         # TODO: Is this the order of processing we want? Is it the same as legacy? I think yes and yes, but CONFIRM!
+        super().update(delta_time, ephase_name)
+
+
+class Weapon(Entity):
+    def __init__(self,
+                 groups,
+                 spec: PlayerSpec,
+                 x: float,
+                 y: float,
+                 direction: pygame.math.Vector2,
+                 speed: float,
+                 ):
+        # self.can_shoot: bool = True
+        # self.laser_shoot_time: int = 0
+        # self.cooldown_duration: int = LASER_COOLDOWN_DURATION  # milliseconds
+        super().__init__(groups, spec, x, y, direction, speed)  # super.update() could be done first before setting all the self.* but for now I have them last.
+
+    # TODO: WEAPON/PROJECTILE BEHAVIORS TBD
+    # def weapon_timer(self):
+    #     if not self.can_shoot:
+    #         current_time = pygame.time.get_ticks()  # Milliseconds since pygame.init() was called.
+    #         if current_time - self.laser_shoot_time >= self.cooldown_duration:
+    #             self.can_shoot = True
+
+    def update(self, delta_time: float, ephase_name: str):
+        enviro_influence(self, ephase_name)
+
+        # TODO: WEAPON/PROJECTILE BEHAVIORS TBD
+        # keys = pygame.key.get_pressed()
+        # recent_keys = pygame.key.get_just_pressed()
+        #
+        # self.dir.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        # self.dir.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        #
+        # self.dir = self.dir.normalize() if self.dir else self.dir
+        #
+        # if recent_keys[pygame.K_SPACE] and self.can_shoot:
+        #     print('fire laser')
+        #     self.can_shoot = False
+        #     self.laser_shoot_time = pygame.time.get_ticks()
+        #
+        # self.laser_timer()
+
         super().update(delta_time, ephase_name)
 
 
@@ -407,6 +520,7 @@ pygame.display.set_caption(GAME_TITLE)
 
 all_sprites: pygame.sprite.Group = pygame.sprite.Group()
 all_players: pygame.sprite.Group = pygame.sprite.Group()
+all_weapons: pygame.sprite.Group = pygame.sprite.Group()
 all_npcs: pygame.sprite.Group = pygame.sprite.Group()
 all_props: pygame.sprite.Group = pygame.sprite.Group()
 
@@ -457,6 +571,20 @@ for i, player_spec in enumerate(player_specs):
                              )  # PyCharm FALSE WARNING HERE (AbstractGroup)
     players.append(player)  # Although considered for removal in lieu of sprite groups, I see reasons to keep such lists.
 
+# INSTANITATE WEAPONS/PROJECTILES - TEMPORARY/EXPERIMENTAL - THIS WILL CHANGE TO 'FIRING' (DYNAMIC INSTANTIATION)
+weapons: list[Weapon] = []
+for i, weapon_spec in enumerate(weapon_specs):
+    weapon_spec['instance_id'] = i
+    imgpath = os.path.join(ASSET_PATH, weapon_spec['img'])
+    weapon: Weapon = Weapon( groups=[all_sprites, all_weapons],
+                             spec=weapon_spec,
+                             x=weapon_spec['x'],
+                             y=weapon_spec['y'],
+                             direction=weapon_spec['d'],
+                             speed=weapon_spec['s'],
+                             )  # PyCharm FALSE WARNING HERE (AbstractGroup)
+    weapons.append(weapon)  # Although considered for removal in lieu of sprite groups, I see reasons to keep such lists.
+
 # INSTANITATE NPCs
 npcs: list[Npc] = []
 for i, npc_spec in enumerate(npc_specs):
@@ -506,7 +634,7 @@ tumbleweed_event = pygame.event.custom_type()
 pygame.time.set_timer(tumbleweed_event, 4500)
 
 
-#   * * * * * *    MAIN LOOP    * * * * * *
+#   * * * * * * *    MAIN LOOP    * * * * * * *
 while running:
     g_delta_time = clock.tick(TICKRATE) / 1000  # Seconds elapsed for a single frame (e.g. - 60 Frm/sec = 0.017 sec/Frm)
 
@@ -542,6 +670,7 @@ while running:
     #   ^ ^ ^ ^ ^ ^    MAIN UPDATE ACTIONS    ^ ^ ^ ^ ^ ^
     all_props.update(g_delta_time, g_ephase_name)
     all_npcs.update(g_delta_time, g_ephase_name)
+    all_weapons.update(g_delta_time, g_ephase_name)
     all_players.update(g_delta_time, g_ephase_name)
 
     # REDRAW THE BACKGROUND
@@ -551,11 +680,12 @@ while running:
     #   | | | | | |    MAIN DRAWING ACTIONS    | | | | | |
     all_props.draw(display_surface)
     all_npcs.draw(display_surface)
+    all_weapons.draw(display_surface)
     all_players.draw(display_surface)
 
     pygame.display.flip()  # Similar to update but not entire screen. TODO: Clarify
 
-    # ______________________________________________    END MAIN LOOP    _______________________________________________
+#   * _ * _ * _ *    END MAIN LOOP    * _ * _ * _ *
 
 
 pygame.quit()
